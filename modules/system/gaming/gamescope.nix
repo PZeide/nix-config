250552a8365ae3feaa5757f41ae71811c906d0e4
Zeide <git.lagging775@passmail.net>
2025-05-06 19:04:31 +1200

    fix: fix gamescope wayland

--- modules/system/gaming/gamescope.nix
+++ modules/system/gaming/gamescope.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options.zeide.gaming.gamescope = with lib; {
@@ -11,6 +12,12 @@
       type = types.bool;
       default = false;
       description = "Whether to use nvidia prime in gamescope session";
+    };
+
+    exposeNvidiaGpu = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether to expose Nvidia GPU to the game (to enable RayTracing and DLSS)";
     };
 
     gamescopeWidth = mkOption {
@@ -36,14 +43,26 @@
     selfConfig = config.zeide.gaming.gamescope;
   in
     lib.mkIf selfConfig.enable {
+      environment.systemPackages = with pkgs; [mangohud];
+
       programs.gamescope = {
         enable = true;
         capSysNice = true;
-        env = lib.mkIf selfConfig.useNvidiaPrime {
-          __NV_PRIME_RENDER_OFFLOAD = "1";
-          __VK_LAYER_NV_optimus = "NVIDIA_only";
-          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        };
+        env =
+          {
+            # FIXME Remove when fixed on stable nvidia
+            VKD3D_DISABLE_EXTENSIONS = "VK_KHR_present_wait";
+          }
+          // lib.mkIf selfConfig.useNvidiaPrime {
+            __NV_PRIME_RENDER_OFFLOAD = "1";
+            __VK_LAYER_NV_optimus = "NVIDIA_only";
+            __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+          }
+          // lib.mkIf selfConfig.exposeNvidiaGpu {
+            PROTON_HIDE_NVIDIA_GPU = 0;
+            PROTON_ENABLE_NVAPI = 1;
+            PROTON_ENABLE_NGX_UPDATER = 1;
+          };
 
         args =
           [
@@ -52,9 +71,9 @@
             "-W ${toString selfConfig.gamescopeWidth}"
             "-H ${toString selfConfig.gamescopeHeight}"
             "-r ${toString selfConfig.gamescopeRefreshRate}"
-            "--expose-wayland"
+            # FIXME Use SDL backend instead of Wayland: https://github.com/ValveSoftware/gamescope/issues/1669
+            "--backend sdl"
             "--rt"
-            "--hdr-enabled"
             "--fullscreen"
             "--generate-drm-mode fixed"
             "--adaptive-sync"
